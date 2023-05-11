f9071c09733d5ebe150e44a9b477cf8647c3248e
david zambrano corral <Yayoamigo>
2023-05-11 16:02:17 -0500

    Just client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
-import Home from "./pages/Home"
+import Home from "./pages/home";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+
 
 
 function App() {
-
-
+  //create routes, if there is no user, redirect to login page
+  const user = true;
   return (
-    <Home/>
-
-  )
+    <Routes>
+      <Route path="/" element={user ? <Home /> : <Navigate to="/register"/>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
