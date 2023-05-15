e5286ee6dd039dd2cb8c8bed4daa6161df7fa0e3
david zambrano corral <Yayoamigo>
2023-05-15 16:11:14 -0500

    Finish front

--- client/src/redux/cartDucks.ts
+++ client/src/redux/cartDucks.ts
@@ -1,30 +1,60 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
-  products: [] as { price: number; quantity: number ; id: number }[],
-  quantity: 0,
-  total: 0
-};
+interface Product {
+  price: number;
+  quantity: number;
+  id: number;
+  uniqueId: string;
+    name: string;
+    img: string;
+}
 
-// Create interface for state
-export interface CartState {
-  products: { price: number; quantity: number; id: number }[];
+interface CartState {
+  products: Product[];
   quantity: number;
   total: number;
 }
+
+const initialState: CartState = {
+  products: [],
+  quantity: 0,
+  total: 0,
+};
+
+const generateUniqueId = (): string => {
+  // Generate a unique identifier using a library or custom logic
+    const uniqueId = Date.now().toString(36);
+    return uniqueId;
+};
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addItem: (state, action: PayloadAction<{ price: number; quantity: number; id: number}>) => {
-    const { price, quantity, id } = action.payload;
+    addItem: (state, action: PayloadAction<{ price: number; quantity: number; id: number, name: string, img: string }>) => {
+      const { price, quantity, id, name, img } = action.payload;
+      const uniqueId = generateUniqueId();
+
       state.quantity += 1;
-      state.products.push({ price: price, quantity: quantity, id: id });
+      state.products.push({ price, quantity, id, uniqueId, name, img });
       state.total += price * quantity;
-    }
-  }
+    },
+    removeItem: (state, action: PayloadAction<{ uniqueId: string; price: number; quantity: number; }>) => {
+      const { uniqueId, price, quantity } = action.payload;
+
+      const newProducts = state.products.filter((product) => product.uniqueId != uniqueId);
+      state.products = newProducts;
+      state.quantity -= quantity;
+      if(state.total > 0){
+      state.total -= price * quantity;
+      } else {
+            state.total = 0;
+      }
+
+      
+    },
+  },
 });
 
-export const { addItem } = cartSlice.actions;
+export const { addItem, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
